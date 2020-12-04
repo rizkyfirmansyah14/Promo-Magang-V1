4c1cf9ca9bb19f90c5806ac917528a167dd0a063
Muhammad Farras Jibran <farasjibran@gmail.com>
2020-12-04 15:17:52 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -43,25 +43,25 @@ Route::post('/admin/edituser', 'UserController@editUser')->name('edituser');
 Route::post('/admin/deleteuser', 'UserController@deleteUser')->name('deleteuser');
 
 // Route CRUD discount
-Route::get('/admin/getdiskon', 'DiskonController@dataDisc');
-Route::post('/admin/adddiskon', 'DiskonController@addDisc');
-Route::post('/admin/getiddiskon', 'DiskonController@getIdDisc');
-Route::post('/admin/editdiskon', 'DiskonController@editDisc');
-Route::post('/admin/deletediskon', 'DiskonController@deleteDisc');
+Route::get('/admin/getdiskon', 'DiskonController@dataDisc')->name('getdiskon');
+Route::post('/admin/adddiskon', 'DiskonController@addDisc')->name('adddiskon');
+Route::post('/admin/getiddiskon', 'DiskonController@getIdDisc')->name('getiddiskon');
+Route::post('/admin/editdiskon', 'DiskonController@editDisc')->name('editdiskon');
+Route::post('/admin/deletediskon', 'DiskonController@deleteDisc')->name('deletediskon');
 
 //Route CRUD kategori
-Route::get('/admin/getkategori', 'KategoriController@dataCat');
-Route::post('/admin/addkategori', 'KategoriController@addCat');
-Route::post('/admin/getidkategori', 'KategoriController@getIdCat');
-Route::post('/admin/editkategori', 'KategoriController@editCat');
-Route::post('/admin/deletekategori', 'KategoriController@deleteCat');
+Route::get('/admin/getkategori', 'KategoriController@dataCat')->name('getkategori');
+Route::post('/admin/addkategori', 'KategoriController@addCat')->name('addkategori');
+Route::post('/admin/getidkategori', 'KategoriController@getIdCat')->name('getidkategori');
+Route::post('/admin/editkategori', 'KategoriController@editCat')->name('editkategori');
+Route::post('/admin/deletekategori', 'KategoriController@deleteCat')->name('deletekategori');
 
 // Route CRUD role
-Route::get('/admin/getrole', 'RoleController@dataRole');
-Route::post('/admin/addrole', 'RoleController@addRole');
-Route::post('/admin/getidrole', 'RoleController@getIdRole');
-Route::post('/admin/editrole', 'RoleController@editRole');
-Route::post('/admin/deleterole', 'RoleController@deleteRole');
+Route::get('/admin/getrole', 'RoleController@dataRole')->name('getrole');
+Route::post('/admin/addrole', 'RoleController@addRole')->name('addrole');
+Route::post('/admin/getidrole', 'RoleController@getIdRole')->name('getidrole');
+Route::post('/admin/editrole', 'RoleController@editRole')->name('editrole');
+Route::post('/admin/deleterole', 'RoleController@deleteRole') - name('deleterole');
 
 // Route CRUD kategori barang
 Route::get('/admin/getktbarang', 'KategoriBarangController@dataKat')->name('getktbarang');
