28caf1e283ff44bb8335cb836dd7fd24610506a4
Muhammad Farras Jibran <farasjibran@gmail.com>
2020-12-08 13:47:45 +0700

    Update about.blade.php

--- resources/views/user/about.blade.php
+++ resources/views/user/about.blade.php
@@ -285,19 +285,6 @@
                 </div>
             </section>
 
-            <!-- News Letter -->
-            <section class="news-letter padding-top-150 padding-bottom-150">
-                <div class="container">
-                    <div class="heading light-head text-center margin-bottom-30">
-                        <h4>NEWSLETTER</h4>
-                        <span>Phasellus lacinia fermentum bibendum. Interdum et malesuada fames ac ante ipsumien lacus, eu posuere odi </span>
-                    </div>
-                    <form>
-                        <input type="email" placeholder="Enter your email address" required>
-                        <button type="submit">SEND ME</button>
-                    </form>
-                </div>
-            </section>
         </div>
 
         <!--======= FOOTER =========-->
