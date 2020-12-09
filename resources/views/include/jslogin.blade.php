e5242f3333cfba8a93061314e7b71acb609bbe55
Muhammad Farras Jibran <farasjibran@gmail.com>
2020-12-09 17:09:17 +0700

    login template update

--- resources/views/include/jslogin.blade.php
+++ resources/views/include/jslogin.blade.php
@@ -1,16 +1,9 @@
-<!--===============================================================================================-->
-<script src="{{ asset('vendor/jquery/jquery-3.2.1.min.js')}}"></script>
-<!--===============================================================================================-->
-<script src="{{ asset('vendor/animsition/js/animsition.min.js')}}"></script>
-<!--===============================================================================================-->
-<script src="{{ asset('vendor/bootstrap/js/popper.js')}}"></script>
-<script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js')}}"></script>
-<!--===============================================================================================-->
-<script src="{{ asset('vendor/select2/select2.min.js')}}"></script>
-<!--===============================================================================================-->
-<script src="{{ asset('vendor/daterangepicker/moment.min.js')}}"></script>
-<script src="{{ asset('vendor/daterangepicker/daterangepicker.js')}}"></script>
-<!--===============================================================================================-->
-<script src="{{ asset('vendor/countdowntime/countdowntime.js')}}"></script>
-<!--===============================================================================================-->
-<script src="{{ asset('js/main.js')}}"></script>
+<!-- Bootstrap core JavaScript-->
+<script src="{{ asset('stylelogin/vendor/jquery/jquery.min.js')}}"></script>
+<script src="{{ asset('stylelogin/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="{{ asset('stylelogin/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="{{ asset('stylelogin/js/sb-admin-2.min.js')}}"></script>
--- resources/views/include/stylelogin.blade.php
+++ resources/views/include/stylelogin.blade.php
@@ -1,20 +1,6 @@
-<!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css')}}">
-<!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{ asset('fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
-<!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{ asset('fonts/Linearicons-Free-v1.0.0/icon-font.min.css')}}">
-<!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{ asset('vendor/animate/animate.css')}}">
-<!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{ asset('vendor/css-hamburgers/hamburgers.min.css')}}">
-<!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{ asset('vendor/animsition/css/animsition.min.css')}}">
-<!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{ asset('vendor/select2/select2.min.css')}}">
-<!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{ asset('vendor/daterangepicker/daterangepicker.css')}}">
-<!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{ asset('css/util.css')}}">
-<link rel="stylesheet" type="text/css" href="{{ asset('css/main.css')}}">
-<!--===============================================================================================-->
+<!-- Custom fonts for this template-->
+<link href="{{ asset('stylelogin/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+<!-- Custom styles for this template-->
+<link href="{{ asset('stylelogin/css/sb-admin-2.min.css')}}" rel="stylesheet">
