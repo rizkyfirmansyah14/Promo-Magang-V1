d8347d3c5df2ef8f354c674a50ae580f20bae4fc
Muhammad Farras Jibran <farasjibran@gmail.com>
2020-12-10 08:18:34 +0700

    update login & add register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,81 +2,90 @@
 <html lang="en">
 
 <head>
-    <title>{{ config('app.name', 'Laravel') }}</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    @include('include.stylelogin')
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Partial Css -->
+    @extends('include.stylelogin')
 
 </head>
 
-<body style="background-color: #666666;">
+<body style="background-color: rgb(229,229,229);">
 
-    <div class="limiter">
-        <div class="container-login100">
-            <div class="wrap-login100">
-                <form class="login100-form validate-form" method="POST" action="{{ route('register')}}" style="margin-top: -5%;">
-                    @csrf
+    <div class="container" style="margin-top: 2%;">
 
-                    <span class="login100-form-title p-b-43">
-                        Register to continue
-                    </span>
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
 
-                    <div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
-                        <input class="input100 @error('name') is-invalid @enderror" type="text" name="name" id="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="Name" style="height: 100%;">
-                        <span class="focus-input100"></span>
+            <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block" style="background-color: rgb(229,229,229);"><i class="fas fa-tags fa-10x" style="margin-top: 45%; margin-left: 30%;"></i></div>
+                            <div class="col-lg-6" style="padding-top: 10%; padding-bottom: 6%;">
+                                <div class="p-5" style="padding-top: 100%;">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Register</h1>
+                                    </div>
+                                    <form class="user" method="POST" action="{{ route('login') }}">
+                                        @csrf
+                                        <div class="form-group">
+                                            <input type="text" class="form-control form-control-user @error('name') is-invalid @enderror" style="border-radius: 10px;" id="name" name="name" value="{{ old('name') }}" placeholder="Enter Your Name..." required autocomplete="name" autofocus>
+                                            @error('name')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                            @enderror
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="email" class="form-control form-control-user @error('email') is-invalid @enderror" style="border-radius: 10px;" id="email" name="email" value="{{ old('email') }}" placeholder="Enter Email Address..." required autocomplete="email" autofocus>
+                                            @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                            @enderror
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" style="border-radius: 10px;" class="form-control form-control-user @error('password') is-invalid @enderror" id="password" name="password" placeholder="Password" required autocomplete="current-password">
+                                            @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                            @enderror
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" style="border-radius: 10px;" class="form-control form-control-user" id="password-confirm" name="password_confirmation" placeholder="Password Confirm" required autocomplete="new-password">
+                                        </div>
+                                        <button type="submit" class="btn btn-user btn-block text-gray-900" style="background-color: rgb(229,229,229); border-radius: 10px;">
+                                            Register
+                                        </button>
+                                        <br>
+                                        <div class="text-center">
+                                            <a class="small" href="{{ route('login') }}">Have An Account? Login Now!</a>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    @error('name')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                    @enderror
-
-                    <div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
-                        <input class="input100 @error('email') is-invalid @enderror" type="text" name="email" id="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email" style="height: 100%;">
-                        <span class="focus-input100"></span>
-                    </div>
-                    @error('email')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                    @enderror
-
-                    <div class="wrap-input100 validate-input" data-validate="Password is required">
-                        <input id="password" class="input100 @error('password') is-invalid @enderror" type="password" name="password" required autocomplete="new-password" placeholder="Password" style="height: 100%;">
-                        <span class="focus-input100"></span>
-                    </div>
-                    @error('password')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                    @enderror
-
-                    <div class="wrap-input100 validate-input" data-validate="Password Confirm is required">
-                        <input id="password-confirm" class="input100 @error('password-confirm') is-invalid @enderror" type="password" name="password_confirmation" required autocomplete="new-password" placeholder="Password Confirm" style="height: 100%;">
-                        <span class="focus-input100"></span>
-                    </div>
-                    @error('password')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                    @enderror
-
-                    <div class="container-login100-form-btn">
-                        <button class="login100-form-btn">
-                            Register
-                        </button>
-                        <a href="{{ route('login')}}" style="margin-top: 20%;">Have an account? Login Now!</a>
-                    </div>
-                </form>
-
-                <div class="login100-more" style="background-image: url('https://images.unsplash.com/photo-1580828343064-fde4fc206bc6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1051&q=80');">
                 </div>
+
             </div>
+
         </div>
+
     </div>
 
-    @include('include.jslogin')
+    <!-- Partial Js -->
+    @extends('include.jslogin')
 
 </body>
 
